d274be93e782b8d6c84f48ddf3255513ab0dbcaa
Alexandra Sivira <kerensivira@gmail.com>
2022-02-06 13:09:05 -0500

    5

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 
 function Cantidad() {
 
+var cantidadDePaletas = document.getElementById('Cantidad');
 var Precio = 2000;
-var cantidadDePaletas = document.getElementsByClassName('cantidad');
-var IVA = 19;
-var div = 100;
-var resultadoSinIva
-var resultadoConIva
 
-resultadoSinIva = parseInt(precio) * parseInt(cantidadDePaletas);
-resultadoConIva = parseInt(resultadoSinIva) * parseInt(IVA) / parseInt(div);
 
-console.log (resultadoConIva)
+const resultado = cantidadDePaletas * Precio;
+
+console.log(resultado);
+
+//var IVA = 19;
+// var div = 100;
+// var resultadoSinIva
+// var resultadoConIva
+
+// resultadoSinIva = parseInt(Precio) * parseInt(cantidadDePaletas);
+//resultadoConIva = parseInt(resultadoSinIva) * parseInt(IVA) / parseInt(div);
+
 }
 
-cantidad();
+Cantidad();
